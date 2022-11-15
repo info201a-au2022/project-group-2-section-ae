9e6735d6ec0a655432a9861c26bf29a21a02c49d
SheaKim <sheakim2@uw.edu>
2022-11-14 19:03:13 -0800

    new data

--- source/chart 1 code.R
+++ source/chart 1 code.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(ggplot2)
 library(dplyr)
 library(stringr)
+library(lintr)
 
 #1: time series, deaths vs victims 2016-2019
 
@@ -11,17 +12,11 @@ victimsdata <- read.csv("../data/childvictimstrend.csv")
 deathsdata <- read.csv("../data/deathsdata.csv")
 alldata <- merge(victimsdata, deathsdata, by = "Year")
 
-# chart1 <- ggplot(victimsdata, aes(x = Year, y = National)) +
-#   geom_line(data = victimsdata, aes(x = Year, y = National), col = "blue") +
-#   geom_line(data = deathsdata, aes(x = Year, y = National * 450), col = "red") +
-#   scale_y_continuous(sec.axis=sec_axis(~./450,name="Deaths")) +
-#   labs(color = "Legend") +
-#   scale_color_manual(values = colors)
-
 
 chart1 <- ggplot(alldata, aes(x = Year)) +
-  geom_bar(aes(y = National.x, fill = "Victims"), stat = "identity", position = "dodge") +
-  geom_bar(aes(y= National.y * 200, fill = "Deaths"), stat = "identity", position = "dodge")+
-  scale_y_continuous(sec.axis=sec_axis(~./200,name= "Deaths"), labs(x = "Victims"))
-
-chart1
+  geom_bar(aes(y = National.x, fill = "Victims"),
+           stat = "identity", position = "dodge") +
+  geom_bar(aes(y = National.y * 200, fill = "Deaths"),
+           stat = "identity", position = "dodge") +
+  scale_y_continuous(sec.axis = sec_axis(~. / 200, name = "Deaths"),
+                     labs(x = "Victims"))
--- source/chart 2 code.R
+++ source/chart 2 code.R
@@ -4,8 +4,6 @@ library(ggplot2)
 library(dplyr)
 library(stringr)
 
-#2: bar graph with percent of children by age
-
 agedata <- read.csv("../data/childvictimsage.csv")
 
 agedata2 <- agedata %>%
@@ -16,5 +14,3 @@ agedata2$Ages <- substr(agedata2$Ages, 1, 2)
 chart2 <- ggplot(agedata2, aes(x = Ages, y = National)) +
   geom_col(fill = "cyan4", col = "black")+
   labs(y = "Rate per 1000 children")
-
-chart2
