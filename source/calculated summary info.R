752bbdf5d4bc40744e0f61fd96b103e27e354b2c
Varsha Venkatesan <varshavenkatesan@Varshas-MacBook-Pro.local>
2022-11-14 19:55:31 -0800

    datasum

--- source/calculated summary info.R
+++ source/calculated summary info.R
@@ -1,25 +1,25 @@
 library(tidyverse)
 
 abuse <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-2-section-ae/main/data/totaldata.csv")
-                  
+
 abusetable <- abuse %>%
   mutate(new_cases = 1) %>%
   group_by(state) %>%
   summarize(cases = sum(new_cases))
-  
-  
-  
+
+
+
 
 
 most_year <- function() {
-   abuse %>%
+  abusetable %>%
     filter(cases == max(cases)) %>%
-    return(year)
+    return(state)
 }
 
+most_year()
 
 most_state <- function() {
   abuse %>%
     filter(cases == max(cases))
 }
-
