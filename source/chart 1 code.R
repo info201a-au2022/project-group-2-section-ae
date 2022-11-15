8aec806ec68faecab294d5e554555a314870208e
SheaKim <sheakim2@uw.edu>
2022-11-14 20:34:37 -0800

    redoing chart 1 code for rmd

--- source/chart 1 code.R
+++ source/chart 1 code.R
@@ -1,17 +1,13 @@
-#library things
 library(tidyverse)
 library(ggplot2)
 library(dplyr)
 library(stringr)
 library(lintr)
 
-#1: time series, deaths vs victims 2016-2019
-
-#read.csv data in
 victimsdata <- read.csv("../data/childvictimstrend.csv")
 deathsdata <- read.csv("../data/deathsdata.csv")
-alldata <- merge(victimsdata, deathsdata, by = "Year")
 
+alldata <- merge(victimsdata, deathsdata, by = "Year")
 
 chart1 <- ggplot(alldata, aes(x = Year)) +
   geom_bar(aes(y = National.x, fill = "Victims"),
