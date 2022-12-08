336310b4ed00b48d6ff1e09d589b35b8bdfb1895
linhchi-le <linhchil@uw.edu>
2022-12-07 19:39:33 -0800

    some report

--- source/app_ui.R
+++ source/app_ui.R
@@ -194,41 +194,133 @@ chart_1 <- tabPanel(
   report_page <- tabPanel(
     "Report",
     titlePanel("Report Page"),
-    h4("Code Name"),
+    h3("Code Name"),
     p("Protect"),
-    h4("Project Title"),
+    h3("Project Title"),
     p("For a Better World: Analysis of Child Abuse Cases and Reports from 
     2015-2019 in the United States"),
-    h4("Authors"),
+    h3("Authors"),
     p("Linh Chi Le, Varsha Venkatesan, Sheamin Kim"),
     p("linhchil@uw.edu, vvenk@uw.edu, sheakim2@uw.edu"),
-    h4("Affiliation"),
+    h3("Affiliation"),
     p("INFO- 201: Technical Foundations of Informatics- The Information School
       - University of Washington"),
-    h4("Date"),
+    h3("Date"),
     p("Autumn 2022"),
-    h4("Abstract"),
+    h3("Abstract"),
     p("Our main question is to ask what the trends regarding different facets 
     of child abuse across the United States are over time. This question is 
     important to understand how, where, and when child abuse happens, so that 
     we can decrease the instances of these cases. To address this question, we 
     will analyze child abuse statistics and data through statistcal analysis 
     and visualizations."),
-    h4("Problem Domain"),
-    p("PUT SOMETHING HERE"),
-    h4("Research Questions"),
-    p("INSERT QUESTIONS AND STUFF"),
-    h4("The Dataset"),
-    h4("Expected Implications"),
-    h4("Limitations"),
-    h4("Findings"),
-    h4("Discussion"),
-    h4("Conclusion"),
-    h4("Acknowledgements"),
+    h3("Keywords"),
+    p("Child abuse, child victims, child neglect, child welfare"),
+    h3("Introduction"),
+    p("For our project we will be looking at different aspects of child abuse,
+      including cases, perpetrators, victim age, and perpetrator relationships
+      to get a better understanding of the existence of child abuse in the 
+      United States before COVID-19. We will be analyzing trends across time
+      (5 years, 2015 to 2019) and across states to find correlations and
+      meaningful statistics. We will also focus on the the location of these
+      abuse cases. By using data visualizations for every facet (and combining
+      facets), we plan to see if there are any serious trends to note. By
+      researching and finding data, we want to understand the cases of child
+      abuse in the United States in depth and we hope creating visualizations
+      will help us understand more about this topic. This project also aims to
+      help educate those who are unaware about the trends of child abuse in the
+      United States."),
+    h3("Problem Domain"),
+    h3("Research Questions"),
+    strong("What type of child abuse is the most common?"),
+    p("By understanding what form of child abuse is the most common, it can help 
+    us be more aware of signs of abuse. It can also help us  understand whether
+    or not a child needs help. Some cases of abuse may go unnoticed, however, 
+    by analyzing the types of abuse and seeing which one is most common, we can
+    use those signs and see the similarities to look for cases that also have 
+    those signs to help the children."),
+    strong("What is the age distribution of child abuse cases?"),
+    p("In our analysis, we have decided that children are people from birth to 
+      the age of 17. We hypothesize that abuse case trends will be different 
+      across different ages. By understanding which age group is most at risk, 
+      we can provide more specialized care for children who are victims of 
+      abuse."),
+    strong("What areas in the United States have the most deaths from child 
+    abuse?"),
+    p("We decided to look into this question because we thought that there may 
+    be patterns in which states have more deaths from child abuse compared to 
+    others. This could either be from the population or the values and beliefs 
+    of some states. There may also not be a pattern. By figuring out what areas
+    in the United States has more deaths from child abuse, we could focus on 
+    them more to prevent more cases and save childen."),
+    h3("The Dataset"),
+    h3("Expected Implications"),
+    p("A possible implication that technologists, designers, and policymakers
+    have is that they could do further research on other factors of child
+    abuse. Designers could inform the public and raise awareness to this topic
+    by creating and designer infographics with information and statistics on
+    child abuse. This could potentially help others be aware of the signs of
+    child abuse. Adding on, policymakers could also promote the importance of
+    being on the lookout for children who could be experiencing abuse. They
+    could also create strict laws to prevent child abuse, or make the harsh
+    punishments for people who hurt children. Places like daycares and people
+    like babysitters are commonly known to have child abuse in some areas, so
+    there could be more rules for those people and places. Technologists could
+    figure out how to lessen the cases of child abuse by using technology to
+    fix problems. With the power of designers, technologists, and policymakers,
+    the issue of child abuse could significantly go away."),
+    h3("Limitations"),
+    p("A limitation that we thought of is that it may be difficult to get an
+    accurate count of how many child abuse cases there are in the United States
+    as of right now. This is because some children may currently be
+    experiencing abuse right now, but nobody could ever know. Another
+    limitation is the definition of abuse. People may have different thoughts
+    about what abuse is, depending on their background, where they grew up,
+    and how they grew up. Some may even not know that they are being abused.
+    They could think that what they are experiencing is normal. This limitation
+    also ties into not knowing the most accurate number of child abuse cases.
+    Our findings could have a lower number of cases than the actual number in
+    real life. Cases that are never found by others or reported by the victims
+    would not appear in databases."),
+    h3("Findings"),
+    p("After completing our Shiny web app, we have been able to figure out the
+    answers to our research questions. Our first question was “What type of
+    child abuse is the most common?”. In our web app, we included a chart that
+    has the number of cases of child abuse for each form of abuse. Users are
+    able to select a state in the United States to see the data for each state.
+    The types that we included were medical neglect, multiple maltreatment
+    types, neglect, other, physical abuse, psychological maltreatment, sex
+    trafficking, sexual abuse, and unknown. Out of the nine forms of abuse, we
+    thought that physical abuse would be the most common. However, we found out
+    that the most common form of abuse was neglect. Physical abuse seemed to be
+    the second most common in some states. Our second research question was
+    “What is the age distribution of child abuse cases?”. We made another chart
+    that displayed the frequency of child victims sorted by their ages.
+    Surprisingly, the ages that had the highest rate of abuse are newborns. 
+    Children that were less than one year old, one year old, and two years old
+    had a higher frequency than those who were around ten or eleven. As the age
+    increased, the rate decreased, which was very interesting, and this trend 
+    was seen in virtually every state. We hypothesized that this is from
+    parents being unable to form a bond with their newborn. It also connects to
+    our first research question, since neglection was the most common type of
+    child abuse. The babies could have been neglected by their parents, because
+    they unfortunately are easier to “forget”. Our final research question was
+    “What areas in the United States have the most deaths from child abuse?”
+    We thought California would have the most deaths because it has the highest
+    population. However, after creating a chart that compares the deaths
+    between two states that the user can put in, we find out that Texas has the
+    most deaths, even though California has a higher population than Texas.
+    This is actually not too surprising because Texas is also a large state
+    with a large population. We think that population plays a role in how many
+    deaths a state has because the states with a lower population had less
+    deaths, while the states with higher populations had more deaths."),
+    h3("Discussion"),
+    h3("Conclusion"),
+    h3("Acknowledgements"),
     p("Special thanks to our teaching assistant Annie Tu for her help and 
       guidance throughout the making of this project."),
-    h4("References"),
-    h4("Appendix A: Questions"),
+    h3("References"),
+    h3("Appendix A: Questions"),
     p("None"),
     
 )
